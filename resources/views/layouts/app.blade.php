2ace0401d5a8c59b030faf397add5c662e82b92e
Shota Kuchiki <kuchiki0602@outlook.jp>
2020-04-11 18:55:48 +0900

    try secure_asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,23 +9,31 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @if(app('env') == 'production')
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>    
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ secure_asset('css/common.css') }}">
+    @else
+        <script src="{{ asset('js/app.js') }}" defer></script>    
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @endif  
 
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body class="h-100">
     <main class="h-100">
         <div id="index" class="h-100"></div>
-        <script src={{ asset('js/app.js') }}></script>
+        @if(app('env') == 'production')
+            <script src={{ secure_asset('js/app.js') }}></script>
+        @else
+            <script src={{ asset('js/app.js') }}></script>
+        @endif  
     </main>
 </body>
 </html>
